a7e649ebb6083f6c89021a65558985dbde3b50b7
giangnmt98 <giangnmt98@gmail.com>
2024-11-25 15:16:46 +0700

    Add support for GIT_URL argument in create_dag.sh
    This update introduces a new `--git_url` argument to the `create_dag.sh` script. The script now validates the presence of this argument and uses it to dynamically name the task operator based on the Git repository's basename.

--- create_dag.sh
+++ create_dag.sh
@@ -3,10 +3,12 @@
 # Initialize variables
 DAG_ID=""
 OUTPUT_FILE=""
+GIT_URL=""
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 --dag_id <dag_id> --file_path <output_python_file_define_dag (must be in the dags folder)>"
+    echo "Usage: $0 --dag_id <dag_id> --file_path <output_python_file_define_dag (must be in the dags folder)> --git_url <url>"
+    echo "Example: $0 --dag_id example_dag --file_path /path/to/dags/example_dag.py --git_url https://github.com/username/repo.git"
     exit 1
 }
 
@@ -15,21 +17,24 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         --dag_id) DAG_ID="$2"; shift ;;
         --file_path) OUTPUT_FILE="$2"; shift ;;
+        --git_url) GIT_URL="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; usage ;;
     esac
     shift
 done
 
-# Check if both arguments are provided
-if [ -z "$DAG_ID" ] || [ -z "$OUTPUT_FILE" ]; then
+# Check if all arguments are provided
+if [ -z "$DAG_ID" ] || [ -z "$OUTPUT_FILE" ] || [ -z "$GIT_URL" ]; then
     usage
 fi
+
+# Extract package name from Git URL
+package_name=$(basename -s .git "$GIT_URL")
 
 # Create the Python file content with the provided DAG ID
 cat <<EOL > "$OUTPUT_FILE"
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
 from airflowproject.configs import conf
 from airflowproject.functions.dag_functions import submit_job
 from airflowproject.functions.task_failure_callback import handle_failure_task
@@ -48,7 +53,7 @@ with DAG(
     catchup=catchup,
 ) as dag:
     # Define the task with clear structure
-    extract_feature_task = PythonOperator(
+    ${package_name}_task = PythonOperator(
         task_id="data_processing_task",
         python_callable=submit_job,
         on_failure_callback=handle_failure_task,
